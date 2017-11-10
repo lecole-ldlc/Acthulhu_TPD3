eddbecee7017d9e4a78e2cf2fb2341a5b8f413d6
corentin.armand <corentin.armand@lecole-ldlc.fr>
2017-11-10 08:53:13 +0100

    Bar chart + tree map fonctionnel

--- js/barcharttime.js
+++ js/barcharttime.js
@@ -1,12 +1,14 @@
-function barcharttime() {
+function draw_barchart(data, element, total) {
     console.log("Barchart task loading");
       // Chart initialization
-  	var svg = d3.select("#barcharttime"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + 	margin.top + ")");
-    console.log(g);
+	$(element).html("");
+  	var svg = d3.select(element),
+        margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + 	margin.top + ")");
+        console.log(g);
+
   	// X axis
     var x = d3.scaleBand()
       .rangeRound([0, width])
@@ -17,24 +19,21 @@ function barcharttime() {
       .rangeRound([height, 0]);
 
   	//load data
-    d3.csv("data/data_task_100.csv", function(data){
-     	console.log(data);
-
-      // Group by status
-      var nested_data = d3.nest()
-				.key(function(d) { return d.status; })
-      	.rollup(function(d){
-          return d3.sum(d, function(g){
-            return g.time
-          })
-        })
-				.entries(data);
-
-      nested_data.forEach(function(d){
-        d.status = d.key;
-        d.sumtime = d.value;
-      });
-      console.log(nested_data);
+    if(!total) {
+        // Group by status
+        var nested_data = d3.nest()
+            .key(function (d) {
+                return d.status;
+            })
+            .entries(data);
+    }
+    else {
+        var nested_data = d3.nest()
+            .key(function (d) {
+                return d.priority;
+            })
+            .entries(data);
+    }
       //console.log(nested_data);
 
       // Color scale
@@ -42,7 +41,7 @@ function barcharttime() {
 
       // Set domains of axes scales
       x.domain(nested_data.map(function(d) { return d.key; }));
-      y.domain([0, d3.max(nested_data, function(d) { return d.sumtime; })]);
+      y.domain([0, d3.max(nested_data, function(d) { return d.values.length; })]);
       z.domain(nested_data.map(function(d) { return d.key; }))
 
       // Draw rect
@@ -50,19 +49,12 @@ function barcharttime() {
     	.data(nested_data)
     	.enter().append("rect")
       .attr("x", function(d) { return x(d.key); })
-      .attr("y", function(d) { return y(d.sumtime); })
-      .attr("height", function(d) { return height - y(d.sumtime);})
+      .attr("y", function(d) { return y(d.values.length); })
+      .attr("height", function(d) { return height - y(d.values.length);})
       .attr("width", x.bandwidth())
       //.attr("fill", "red")
       .attr("fill", function(d,i) { return z(d.key); })
-      .on("mouseover", function() { tooltip.style("display", null); })
-  		.on("mouseout", function() { tooltip.style("display", "none"); })
-      .on("mousemove", function(d) {
-    		var xPosition = d3.mouse(this)[0] - 0;
-    		var yPosition = d3.mouse(this)[1] - 25;
-    		tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-    		tooltip.select("text").text(d.sumtime);
-       });
+
 
 
       // Draw x axis
@@ -78,22 +70,6 @@ function barcharttime() {
 
 
 
-    });
-		// Prep the tooltip bits, initial display is hidden
-		var tooltip = svg.append("g")
-  		.attr("class", "tooltip")
-  		.style("display", "none");
-
-		tooltip.append("rect")
-  		.attr("width", 30)
-  		.attr("height", 20)
-  		.attr("fill", "white")
-  		.style("opacity", 0.5);
-
-		tooltip.append("text")
-  		.attr("x", 15)
-  		.attr("dy", "1.2em")
-  		.style("text-anchor", "middle")
-  		.attr("font-size", "12px")
-  		.attr("font-weight", "bold");
 }
+		// Prep the tooltip bits, initial display is hidden
+
